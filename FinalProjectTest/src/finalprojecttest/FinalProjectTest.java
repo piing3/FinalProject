45b94442a9bae0754218b4cd6936709d2433cdd4
piing3 <dhblue015@gmail.com>
2015-05-06 18:02:50 -0500

    Added Our Work

---
+++ FinalProjectTest/src/finalprojecttest/FinalProjectTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojecttest;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Davin
+ */
+public class FinalProjectTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        while (true){
+            JOptionPane.showMessageDialog(null, "Ben sucks");
+        }
+    }
+    
+}
