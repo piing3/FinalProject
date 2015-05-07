14e71fbdbdf3402144ff07c1e7f337c2f7c04230
piing3 <dhblue015@gmail.com>
2015-05-06 19:57:58 -0500

    "Map" Update
    Added a keylistener to map

--- FinalProjectTest/src/finalprojecttest/Map.java
+++ FinalProjectTest/src/finalprojecttest/Map.java
@@ -5,6 +5,9 @@
  */
 package finalprojecttest;
 
+import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 /**
@@ -13,15 +16,39 @@ import javax.swing.JFrame;
  */
 class Map extends JFrame{
     
+    private final Container container;
+    
     Map(int fullscreen){
         if(fullscreen == 0){
-            this.setExtendedState(this.MAXIMIZED_BOTH);
+            this.setExtendedState(Map.MAXIMIZED_BOTH);
             this.setUndecorated(true);
         }
         else{
             this.setSize(1280,720);
         }
+        
         this.setVisible(true);
+        this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        container = getContentPane();
+        KeyListener keylistener = new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.out.println("poop");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        container.addKeyListener(keylistener);
     }    
 }
