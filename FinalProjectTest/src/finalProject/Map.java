72a66eb10ec59510c13da682ecf657c81aecbe12
piing3 <dhblue015@gmail.com>
2015-05-07 11:26:30 -0500

    Map update, Menu creation and Renaming
    Fixed the key listener, added an exit button, and started working on the
    escape key menu. Also fixed the file names.

---
+++ FinalProjectTest/src/finalProject/Map.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalProject;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.plaf.basic.BasicTreeUI;
+
+/**
+ *
+ * @author Davin
+ */
+class Map extends JFrame implements KeyListener{
+    
+    private final Container container;
+    private Menu menu = new Menu();
+    private JButton btnExit;
+    boolean menuOpen = false;
+    
+    Map(int fullscreen){
+        if(fullscreen == 0){
+            this.setExtendedState(Map.MAXIMIZED_BOTH);
+            this.setUndecorated(true);
+        }
+        else{
+            this.setSize(1280,720);
+        }
+        
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        container = getContentPane();
+        container.setLayout(null);
+        this.addKeyListener(this);
+        
+        
+        btnExit = new JButton("X");
+        btnExit.setBounds(0, 0, 50, 50);
+        btnExit.setBackground(Color.red);
+        btnExit.setForeground(Color.white);
+        btnExit.setFocusable(false);
+        container.add(btnExit);
+        ActionListener Exit = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        };
+        btnExit.addActionListener(Exit);
+    }    
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println(e.getKeyChar() +" "+ e.getKeyCode());
+        if (e.getKeyCode() == 27){
+            if (menuOpen == false){
+                menu.setVisible(true);
+                System.out.println("Menu open");
+                menuOpen = true;
+            }
+
+            else if (menuOpen == true){
+                menu.setVisible(false);
+                System.out.println("Menu closed");
+                menuOpen = false;
+            }
+            
+        }
+    }
+}
