c1f45888b101b378180fa06d631d52eebd911916
piing3 <d.holmberg@sjasd.ca>
2017-02-07 11:01:47 -0600

    Map Code Formating

--- FinalProjectTest/src/finalProject/Map.java
+++ FinalProjectTest/src/finalProject/Map.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finalProject;
 
 import java.io.File;
@@ -38,15 +33,15 @@ class Map {
         borderGrid  = new Border[WIDTH][HEIGHT];
         borderType  = new int[WIDTH][HEIGHT];
                 
-        File temp = new File("src\\finalProject\\Map.txt");//reads the map and stores it in tileType, also sets all borderType to 0
-        Scanner s = new Scanner(temp);
-        for (int j = 0; j <= HEIGHT-1; j++){
-            for (int i = 0; i <= WIDTH-1; i++){
-                borderType[i][j] = 0;
-                tileType[i][j] = s.nextInt();
-            }
-        }
-        s.close(); 
+//        File temp = new File("src\\finalProject\\Map.txt");//reads the map and stores it in tileType, also sets all borderType to 0
+//        Scanner s = new Scanner(temp);
+//        for (int j = 0; j <= HEIGHT-1; j++){
+//            for (int i = 0; i <= WIDTH-1; i++){
+//                borderType[i][j] = 0;
+//                tileType[i][j] = s.nextInt();
+//            }
+//        }
+//        s.close(); 
         
         for (int i = Visual.rightOff; i < WIDTH-1; i++){
             for (int j = Visual.downOff; j < HEIGHT-1; j++){
